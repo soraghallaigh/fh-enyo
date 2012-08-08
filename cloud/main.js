7fd147d4917101ea87d430531b3fc332237f5c07
fhdemo2@example.com <fhdemo2@example.com>
2012-08-08 12:32:45 +0000

    update file /cloud/main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,27 +1,4 @@
 
-
-function getFeed(params) {
-	var res = {
-		msg: "Invalid parameters",
-		status: "error"
-	};
-
-	if(params.link && params.max) {
-		res = $fh.feed({
-			link: params.link,
-			"list-max" : params.max
-		});
-
-		if(res.list && res.list.length === 0) {
-			res = {
-				status: "error",
-				msg: "Feed does not exist"
-			};
-		}
-	}
-
-	return res;
-};
 exports.getFeed = function(params) {
   var res = {
   	msg: "Invalid parameters",
@@ -46,15 +23,4 @@ exports.getFeed = function(params) {
       
 		});
 	}
-	return res;
 }
-exports.feedCall = function(params, callback) {
-  var feedParams = {          
-    'link': 'http://www.feedhenry.com/feed',
-    'list-max' : 10
-  };
-  console.log("in feedCall");
-  $fh.feed(feedParams, function(err, feedResp) {
-    callback(err, feedResp);
-  });
-};
