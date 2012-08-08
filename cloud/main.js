2adf9f7cfa4ac7984858f37e4f0d87fbf96df318
fhdemo2@example.com <fhdemo2@example.com>
2012-08-08 12:32:03 +0000

    update file /cloud/main.js

--- cloud/main.js
+++ cloud/main.js
@@ -29,19 +29,22 @@ exports.getFeed = function(params) {
 	};
 
 	if(params.link && params.max) {
-		res = $fh.feed({
+		$fh.feed({
 			link: params.link,
 			"list-max" : params.max
+		}, function(err, res) {
+      if(err) {
+        return cb(err);
+      }
+  	  if(res.list && res.list.length === 0) {
+  		  cb(null, {
+				  status: "error",
+				  msg: "Feed does not exist"
+			  });
+		  }
+      
 		});
-
-		if(res.list && res.list.length === 0) {
-			res = {
-				status: "error",
-				msg: "Feed does not exist"
-			};
-		}
 	}
-
 	return res;
 }
 exports.feedCall = function(params, callback) {
